102b67e8ca0a488df9d4d28b4186c83da19a0da2
Clara Hernández <clara.herni@gmail.com>
2020-05-22 18:03:11 -0300

    terminada
    Acomode las lineas para que no esté todo junto.

--- tareas/clase-5/tarea2.js
+++ tareas/clase-5/tarea2.js
@@ -1,10 +1,11 @@
 document.querySelector('#mostrar-texto').onclick = function () {
-    const nuestroTitulo = document.querySelector('h1');
-    const nuestroParrafo = document.querySelector('p');
-    const nombreUsuario = document.querySelector('#nombre-usuario').value;
-    const edadUsuario = document.querySelector('#edad-usuario').value;
+    const $nuestroTitulo = document.querySelector('h1');
+    const $nuestroParrafo = document.querySelector('p');
 
-    nuestroTitulo.innerText = `Bienvenido, ${nombreUsuario}!`;
-    nuestroParrafo.innerText = `Tu nombre es ${nombreUsuario} y tenés ${edadUsuario} años.`;
+    const $nombreUsuario = document.querySelector('#nombre-usuario').value;
+    const $edadUsuario = document.querySelector('#edad-usuario').value;
+
+    $nuestroTitulo.innerText = `Bienvenido, ${$nombreUsuario}!`;
+    $nuestroParrafo.innerText = `Tu nombre es ${$nombreUsuario} y tenés ${$edadUsuario} años.`;
     return false;
 };
