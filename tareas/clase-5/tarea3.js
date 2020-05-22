9438265d3aae531c776cf4d3e92d63eeb18adc90
Clara Hernández <clara.herni@gmail.com>
2020-05-22 18:04:18 -0300

    agrego contador de tiempo y boton de reset
    Ahora el tiempo del video se acumula y se muestra en el texto a medida que se actualiza. También se agrega botón para resetear el tiempo

--- tareas/clase-5/tarea3.js
+++ tareas/clase-5/tarea3.js
@@ -1,1 +1,34 @@
-tarea3.js;
+let horasTotales = 0;
+let minutosTotales = 0;
+let segundosTotales = 0;
+
+const $botonAgregarVideo = document.querySelector('#calcular-tiempo');
+const $botonResetTiempo = document.querySelector('#reset-tiempo');
+
+const $resultado = document.querySelector('#resultado');
+
+$botonAgregarVideo.onclick = function () {
+    let $horasAux, $minutosAux, $segundosAux;
+
+    $minutosAux = Number(document.querySelector('#minutos').value);
+    $segundosAux = Number(document.querySelector('#segundos').value);
+    $horasAux = Number(document.querySelector('#horas').value);
+
+    segundosTotales = $segundosAux % 60;
+
+    minutosAux = Math.floor($segundosAux / 60) + $minutosAux;
+    minutosTotales = $minutosAux % 60;
+
+    horasTotales = horasTotales + $horasAux + Math.floor($minutosAux / 60);
+
+    $resultado.innerText = `El total de tiempo fue ${horasTotales} horas, ${minutosTotales} minutos y ${segundosTotales} segundos.`;
+
+    return false;
+};
+
+$botonResetTiempo.onclick = function () {
+    horasTotales = 0;
+    minutosTotales = 0;
+    segundosTotales = 0;
+    $resultado.innerText = '';
+};
